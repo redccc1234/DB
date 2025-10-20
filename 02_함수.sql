dbe65e1bf2e627e224c3dd28ae9efcc6eaa79230
redccc1234 <redccc1234@gmail.com>
2025-10-20 12:04:35 +0900

    그룹 함수

--- 02_함수.sql
+++ 02_함수.sql
@@ -295,3 +295,50 @@ CASE WHEN SALARY >= 5000000 THEN '대' -- IF
 	 ELSE '소' -- ELSE 
 END "급여 받는 정도"
 FROM EMPLOYEE;
+
+---------------------------------------------------------------------
+
+-- 그룹 함수
+-- 하나 이상의 행을 그룹으로 묶어 연산하여 총합, 평균 등의
+-- 1개의 결과 행으로 반환하는 함수
+
+-- SUM(숫자가 기록된 컬럼명) : 합계
+-- 모든 직원의 급여 합 조회
+SELECT SUM(SALARY) FROM EMPLOYEE; -- 70,096,240
+
+-- ANG(숫자가 기록된 컬럼명) : 평균
+-- 전 직원 급여 평균 조회
+SELECT ROUND(AVG(SALARY)) FROM EMPLOYEE; -- 3,047,662
+
+-- 부서코드가 'D9'인 사원들의 급여 합, 평균 조회
+/*3*/SELECT SUM(SALARY) , ROUND(AVG(SALARY))
+/*1*/FROM EMPLOYEE
+/*2*/WHERE DEPT_CODE = 'D9';
+
+-- MIN(컬럼명) : 최소값
+-- MAX(컬럼명) : 최대값
+--> 타입 제한 없음(숫자 : 대/소, 날짜 : 과거/미래 , 문자열 : 문자 순서 )
+
+-- 급여 최소값, 가장 빠른 입사일, 알파벳 순서가 가장 빠른 이메일 조회
+SELECT MIN(SALARY),MIN(HIRE_DATE),MIN(EMAIL)
+FROM EMPLOYEE;
+
+-- 급여 최대값, 가장 늦은 입사일, 알파벳 순서가 가장 늦은 이메일 조회
+SELECT MAX(SALARY),MAX(HIRE_DATE),MAX(EMAIL)
+FROM EMPLOYEE;
+
+-- EMPLOYEE 테이블에서
+-- 급여를 가장 많이 받는 사원의
+-- 이름,급여,직급코드를 조회
+
+-- 가장 큰 급여 조회
+SELECT MAX(SALARY) FROM EMPLOYEE; -- 서브쿼리 + 그룹함수
+SELECT EMP_NAME,SALARY,JOB_CODE
+FROM EMPLOYEE
+WHERE SALARY = (SELECT MAX(SALARY) FROM EMPLOYEE);
+
+-- COUNT() : 행 개수를 헤아려서 반환
+-- COUNT(컬럼명) : NULL을 제외한 실제값이 기록된 행 개수를 반환
+-- COUNT(*) : NULL을 포함한 전체 행 개수를 반환
+-- COUNT(DISTINCT 컬럼명) : 중복을 제거한 행 개수를 반환
+ SELECT COUNT(*) FROM EMPLOYEE; --23
